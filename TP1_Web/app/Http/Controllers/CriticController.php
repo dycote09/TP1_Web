293b1bd5378572bb413296e740ef9e781228d9d0
dycote09 <dycote09@outlook.com>
2023-04-09 22:32:39 -0400

    avancement auth

--- TP1_Web/app/Http/Controllers/CriticController.php
+++ TP1_Web/app/Http/Controllers/CriticController.php
@@ -19,4 +19,29 @@ class CriticController extends Controller
         $critic = Critic::find($id);
         echo($critic->comment . ' ' . $critic->score . '<br>');
     }
+
+    public function store(Request $request)
+    {
+        $user = $request->user();
+    
+        $film_id = $request->input('film_id');
+        $score = $request->input('score');
+        $comment = $request->input('comment');
+    
+        //condition de 1 critic par film
+        $criticBD = $user->critics()->where('film_id', $film_id)->first();
+        if ($criticBD) {
+            return response()->json(['error' => 'Vous avez déjà écrit une critique pour ce film.'], 400);
+        }
+    
+        $critic = new Critic();
+        $critic->user_id = $user->id;
+        $critic->film_id = $film_id;
+        $critic->score = $score;
+        $critic->comment = $comment;
+        $critic->save();
+    
+        return response()->json(['message' => 'Critique ajoutée avec succès.'], 200);
+    }
+
 }
--- TP1_Web/routes/api.php
+++ TP1_Web/routes/api.php
@@ -19,16 +19,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Routes pour actors
-Route::get('actors','App\Http\Controllers\ActorController@index');
-Route::get('actors/{id}','App\Http\Controllers\ActorController@show');
-
-
-//Routes pour films
+//Route publique
 Route::get('films','App\Http\Controllers\FilmController@index');
 Route::get('films/{id}','App\Http\Controllers\FilmController@show');
-
-
-//Routes pour languages
 Route::get('languages','App\Http\Controllers\LanguageController@index');
+Route::get('actors','App\Http\Controllers\ActorController@index');
+Route::get('actors/{id}','App\Http\Controllers\ActorController@show');
 Route::get('languages/{id}','App\Http\Controllers\LanguageController@show');
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+
+//Route protegé
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('critics', [CriticController::class, 'store']);
+});
+
+Route::middleware('auth:sanctum')->delete('/logout', function (Request $request) {
+    //déconnexion
+});
